b75cd91f8bb90b36529ad4a217417ebae90cd273
Kim Lindhard <kilin@dfds.com>
2018-10-02 12:54:30 +0200

    add container registry webapi

---
+++ src/ContainerRegistry.WebApi/Controllers/CanaryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContainerRegistry.WebApi.Controllers
+{
+    [Route("system/[controller]")]
+    [ApiController]
+    public class CanaryController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
