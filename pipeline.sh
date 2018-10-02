eacdc389d47eabe54a8b5435e4f5adf9d6c1e6ca
Kim Lindhard <kilin@dfds.com>
2018-10-02 14:07:35 +0200

    add pipeline.sh & docker file

---
+++ pipeline.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# build.sh(1)
+#
+
+[[ -n $DEBUG ]] && set -x
+set -eu -o pipefail
+
+# build parameters
+readonly REGION=${AWS_DEFAULT_REGION:-"eu-central-1"}
+readonly IMAGE_NAME='container-registry-service'
+readonly BUILD_NUMBER=${1:-"N/A"}
+readonly BUILD_SOURCES_DIRECTORY=${2:-${PWD}}
+readonly SOLUTION_NAME='ContainerRegistry.sln';
+
+restore_dependencies() {
+    echo "Restoring dependencies"
+    dotnet restore ${SOLUTION_NAME}
+}
+
+publish_binaries() {
+    echo "Publishing binaries..."
+    dotnet publish -c Release -o ${BUILD_SOURCES_DIRECTORY}/output ContainerRegistry.WebApi/ContainerRegistry.WebApi.csproj
+}
+
+
+build_container_image() {
+    echo "Building container image '${IMAGE_NAME}'..."
+    
+    docker build -t ${IMAGE_NAME}:latest .
+}
+
+push_container_image() {
+    echo "Login to docker..."
+    $(aws ecr get-login --no-include-email)
+
+    account_id=$(aws sts get-caller-identity --output text --query 'Account')
+    image_name="${account_id}.dkr.ecr.${REGION}.amazonaws.com/${IMAGE_NAME}:${BUILD_NUMBER}"
+
+    echo "Tagging container image..."
+    docker tag ${IMAGE_NAME}:latest ${image_name}
+
+    echo "Pushing container image to ECR..."
+    docker push ${image_name}
+}
+
+
+cd ./src
+
+restore_dependencies
+publish_binaries
+
+cd ..
+
+build_container_image
+
+if [[ "${BUILD_NUMBER}" != "N/A" ]]; then
+    push_container_image
+fi
